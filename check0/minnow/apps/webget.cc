e67e0a588e89e870cd4befb25eabed14950405d5
huibosa_arch <huibosa@gmail.com>
2024-02-27 07:20:38 +0800

    feat(webget): finish

--- check0/minnow/apps/webget.cc
+++ check0/minnow/apps/webget.cc
@@ -9,8 +9,25 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  // cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  // cerr << "Warning: get_URL() has not been implemented yet.\n";
+
+  string buf {};
+  vector<string> request
+    = { "GET " + path + " HTTP/1.1\r\n", "Host: " + host + "\r\n", "Connection: close\r\n", "\r\n" };
+
+  auto sock = TCPSocket();
+  Address address( host, "http" );
+
+  sock.connect( address );
+  sock.write( request );
+
+  while ( !sock.eof() ) {
+    sock.read( buf );
+    std::cout << buf;
+  }
+
+  sock.shutdown( SHUT_RDWR );
 }
 
 int main( int argc, char* argv[] )
